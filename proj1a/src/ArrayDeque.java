48ed430912a5c054ee98e9863a277f9fec677faa
Hollen <yujianni666@icloud.com>
2022-05-27 14:30:49 -0400

    Construct the array deque

--- proj1a/src/ArrayDeque.java
+++ proj1a/src/ArrayDeque.java
@@ -1,3 +1,12 @@
-public class ArrayDeque {
+public class ArrayDeque <T> {
+    private T[] items;
+    private int size;
+    public ArrayDeque() {
+        items = (T []) new Object[8];
+        size = 0;
+    }
 
+    public ArrayDeque(ArrayDeque other) {
+
+    }
 }
