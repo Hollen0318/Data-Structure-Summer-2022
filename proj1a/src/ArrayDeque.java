5a8e397cb71da4b4b3aaf0c239f653c9313c573c
Hollen <yujianni666@icloud.com>
2022-05-27 14:35:12 -0400

    APIs

--- proj1a/src/ArrayDeque.java
+++ proj1a/src/ArrayDeque.java
@@ -9,4 +9,38 @@ public class ArrayDeque <T> {
     public ArrayDeque(ArrayDeque other) {
 
     }
+
+    public void addFirst(T item) {
+        items[size] = item;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        items[size] = item;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void printDeque() {
+        System.out.println("Print the deque");
+    }
+
+    public T removeFirst() {
+        return items[1];
+    }
+
+    public T removeLast() {
+        return items[1];
+    }
+
+    public T get(int index) {
+        return items[1];
+    }
 }
