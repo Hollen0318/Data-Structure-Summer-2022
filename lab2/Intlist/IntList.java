3b63c9578cac075aa3574a9fd2ecb780147ba43f
Hollen <yujianni666@icloud.com>
2022-05-31 21:42:18 -0400

    IntList catenate method completed

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -1,41 +1,18 @@
 import java.util.Formatter;
-
 /**
- * A naked recursive list of integers, similar to what we saw in lecture 3, but
- * with a large number of additional methods.
- *
  * @author P. N. Hilfinger, with some modifications by Josh Hug and melaniecebula
- *         [Do not modify this file.]
  */
 public class IntList {
-    /**
-     * First element of list.
-     */
     public int first;
-    /**
-     * Remaining elements of list.
-     */
     public IntList rest;
-
-    /**
-     * A List with first FIRST0 and rest REST0.
-     */
     public IntList(int first0, IntList rest0) {
         first = first0;
         rest = rest0;
     }
-
-    /**
-     * A List with null rest, and first = 0.
-     */
     public IntList() {
     /* NOTE: public IntList () { }  would also work. */
         this(0, null);
     }
-
-    /**
-     * Returns a list equal to L with all elements squared. Destructive.
-     */
     public static void dSquareList(IntList L) {
 
         while (L != null) {
@@ -43,10 +20,6 @@ public class IntList {
             L = L.rest;
         }
     }
-
-    /**
-     * Returns a list equal to L with all elements squared. Non-destructive.
-     */
     public static IntList squareListIterative(IntList L) {
         if (L == null) {
             return null;
@@ -61,25 +34,12 @@ public class IntList {
         }
         return res;
     }
-
-    /**
-     * Returns a list equal to L with all elements squared. Non-destructive.
-     */
     public static IntList squareListRecursive(IntList L) {
         if (L == null) {
             return null;
         }
         return new IntList(L.first * L.first, squareListRecursive(L.rest));
     }
-
-    /** DO NOT MODIFY ANYTHING ABOVE THIS LINE! */
-
-
-    /**
-     * Returns a list consisting of the elements of A followed by the
-     * *  elements of B.  May modify items of A. Don't use 'new'.
-     */
-
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         IntList LAST_A_POINTER = A.rest;
@@ -89,47 +49,28 @@ public class IntList {
         LAST_A_POINTER.rest = B;
         return A;
     }
-
-    /**
-     * Returns a list consisting of the elements of A followed by the
-     * * elements of B.  May NOT modify items of A.  Use 'new'.
-     */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList combo = new IntList(A.first,null);
+        IntList combo_POINTER = combo;
+        IntList A_POINTER = A.rest;
+        IntList B_POINTER = B;
+        while (A_POINTER != null) {
+            combo_POINTER.rest= new IntList(A_POINTER.first, null);
+            combo_POINTER = combo_POINTER.rest;
+            A_POINTER = A_POINTER.rest;
+        }
+        while (B_POINTER != null) {
+            combo_POINTER.rest = new IntList(B_POINTER.first, null);
+            combo_POINTER = combo_POINTER.rest;
+            B_POINTER = B_POINTER.rest;
+        }
+        return combo;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     * DO NOT MODIFY ANYTHING BELOW THIS LINE! Many of the concepts below here
-     * will be introduced later in the course or feature some form of advanced
-     * trickery which we implemented to help make your life a little easier for
-     * the lab.
-     */
-
     @Override
     public int hashCode() {
         return first;
     }
-
-    /**
-     * Returns a new IntList containing the ints in ARGS. You are not
-     * expected to read or understand this method.
-     */
     public static IntList of(Integer... args) {
         IntList result, p;
 
@@ -145,12 +86,6 @@ public class IntList {
         }
         return result;
     }
-
-    /**
-     * Returns true iff X is an IntList containing the same sequence of ints
-     * as THIS. Cannot handle IntLists with cycles. You are not expected to
-     * read or understand this method.
-     */
     public boolean equals(Object x) {
         if (!(x instanceof IntList)) {
             return false;
@@ -168,19 +103,6 @@ public class IntList {
         }
         return true;
     }
-
-    /**
-     * If a cycle exists in the IntList, this method
-     * returns an integer equal to the item number of the location where the
-     * cycle is detected.
-     * <p>
-     * If there is no cycle, the number 0 is returned instead. This is a
-     * utility method for lab2. You are not expected to read, understand, or
-     * even use this method. The point of this method is so that if you convert
-     * an IntList into a String and that IntList has a loop, your computer
-     * doesn't get stuck in an infinite loop.
-     */
-
     private int detectCycles(IntList A) {
         IntList tortoise = A;
         IntList hare = A;
@@ -234,6 +156,15 @@ public class IntList {
         }
         out.format(")");
         return out.toString();
+    }
+
+    public static void main(String[] args) {
+        IntList a = new IntList(2,null);
+        a.rest = new IntList(4,null);
+        a.rest = new IntList(6,null);
+        IntList c = new IntList(87,null);
+        c.rest = new IntList(23,null);
+        IntList d = catenate(a,c);
     }
 }
 
